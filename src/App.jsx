1f38a5731b2ad2822995a92b0acb0efd71a75ed6
cardioyac <yac.chenna@gmail.com>
2026-08-14 23:59:25 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useMemo, useEffect } from "react";
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://gkkkiwvbvubzazdpjpea.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdra2tpd3ZidnViemF6ZHBqcGVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1NTA4NjYsImV4cCI6MjA5ODEyNjg2Nn0.hoODoh83OQawIpqjTvq3Y0vQaPVZFPsoWwkKN2LSZCs";
